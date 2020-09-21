6cfb5e859b4c593f7f1e4d126e24572c8b20b65f
Wendell Arnald <wendell.arnald@gmail.com>
2020-09-21 22:48:34 +0100

    Add android app

---
+++ lib/ui/android/material-app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:imc/ui/android/pages/home.page.dart';
+
+class MyMaterialApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
