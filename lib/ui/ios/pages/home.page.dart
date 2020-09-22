06a1388da28721c95c7b12a1b84d8473d04084d3
Wendell Arnald <wendell.arnald@gmail.com>
2020-09-22 18:10:17 +0100

    ios layout

--- lib/ui/ios/pages/home.page.dart
+++ lib/ui/ios/pages/home.page.dart
@@ -13,7 +13,37 @@ class _HomePageState extends State<HomePage> {
         middle: Text("BMI calculation"),
       ),
       child: ListView(
-        children: <Widget>[],
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: CupertinoTextField(
+              placeholder: "Your Height",
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: CupertinoTextField(
+              placeholder: "Your Weight",
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "You're out of shape",
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+              padding: EdgeInsets.all(20),
+              child: CupertinoButton.filled(
+                child: Text(
+                  "Calculation",
+                ),
+                onPressed: () {},
+              ))
+        ],
       ),
     );
   }
